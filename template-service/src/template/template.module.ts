be1cd958cb25f8c1583afc5b96aea963e43b450d
PaulAjii <paulajii134@gmail.com>
2025-11-11 17:24:07 +0100

    feat(template_module): import entities into module

--- template-service/src/template/template.module.ts
+++ template-service/src/template/template.module.ts
@@ -3,9 +3,10 @@ import { TemplateController } from './template.controller';
 import { TemplateService } from './template.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Template_Definition } from '../entities/template_definitions.entity';
+import { Template_Version } from '../entities/template_versions.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Template_Definition])],
+  imports: [TypeOrmModule.forFeature([Template_Definition, Template_Version])],
   controllers: [TemplateController],
   providers: [TemplateService],
 })
