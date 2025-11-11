beb5a9a3ca1e9e7d30b0e1ab38d36ad9a44e73cb
PaulAjii <paulajii134@gmail.com>
2025-11-11 17:52:33 +0100

    feat(template_controller): add POST route to create template definition

--- template-service/src/template/template.controller.ts
+++ template-service/src/template/template.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { TemplateService } from './template.service';
+import { Template_Definition_Dto } from './dtos';
 
-@Controller('template')
-export class TemplateController {}
+@Controller('api/v1/templates')
+export class TemplateController {
+  constructor(private readonly template_service: TemplateService) {}
+
+  @Post()
+  create_template_definition(
+    @Body() template_definition: Template_Definition_Dto,
+  ) {
+    return this.template_service.create_template_definition(
+      template_definition,
+    );
+  }
+}
