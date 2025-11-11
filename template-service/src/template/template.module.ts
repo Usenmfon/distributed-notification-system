ec43ffa0f97c70f971a0aedb09a8df5ca9d4c70b
PaulAjii <paulajii134@gmail.com>
2025-11-11 17:09:38 +0100

    feat(template_module): add imports to template module

--- template-service/src/template/template.module.ts
+++ template-service/src/template/template.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TemplateController } from './template.controller';
 import { TemplateService } from './template.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Template_Definition } from '../entities/template_definitions.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Template_Definition])],
   controllers: [TemplateController],
   providers: [TemplateService],
 })
