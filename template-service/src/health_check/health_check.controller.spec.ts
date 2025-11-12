4b95797290181f2f5dc9b9ee759721a1dc56068b
PaulAjii <paulajii134@gmail.com>
2025-11-12 17:19:45 +0100

    feat(test): add tests for health status check

--- template-service/src/health_check/health_check.controller.spec.ts
+++ template-service/src/health_check/health_check.controller.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthCheckController } from './health_check.controller';
+import {
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+  HealthCheckResult,
+} from '@nestjs/terminus';
 
-describe('HealthCheckController', () => {
+const mockHealthCheckService = {
+  check: jest.fn(),
+};
+
+const mockTypeOrmHealthIndicator = {
+  pingCheck: jest.fn(),
+};
+
+describe('HealthController', () => {
   let controller: HealthCheckController;
+  let health: HealthCheckService;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthCheckController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: mockHealthCheckService,
+        },
+        {
+          provide: TypeOrmHealthIndicator,
+          useValue: mockTypeOrmHealthIndicator,
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthCheckController>(HealthCheckController);
+    health = module.get<HealthCheckService>(HealthCheckService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('check', () => {
+    it('should call the HealthCheckService and return a health check result', async () => {
+      const mockResult: HealthCheckResult = {
+        status: 'ok',
+        info: { database: { status: 'up' } },
+        error: {},
+        details: { database: { status: 'up' } },
+      };
+
+      mockHealthCheckService.check.mockResolvedValue(mockResult);
+
+      const result = await controller.check();
+
+      expect(result).toEqual(mockResult);
+
+      expect(health.check).toHaveBeenCalled();
+
+      expect(health.check).toHaveBeenCalledWith(
+        expect.arrayContaining([expect.any(Function)]),
+      );
+    });
   });
 });
