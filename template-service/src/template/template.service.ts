2e30e9dedb060fd368146f1556716743f8627d5d
PaulAjii <paulajii134@gmail.com>
2025-11-12 13:48:39 +0100

    feat: add get active template version

--- template-service/src/template/template.service.ts
+++ template-service/src/template/template.service.ts
@@ -9,6 +9,7 @@ import { Repository, EntityManager } from 'typeorm';
 import { Template_Definition } from '../entities/template_definitions.entity';
 import { Template_Version } from '../entities/template_versions.entity';
 import { Template_Definition_Dto, Template_Version_Dto } from './dtos';
+import { Active_Template_Query_Dto } from './dtos/active_template.dto';
 
 @Injectable()
 export class TemplateService {
@@ -57,45 +58,45 @@ export class TemplateService {
 
   async activate_version(template_version_id: string) {
     return this.entity_manager.transaction(async (manager) => {
-      const version_to_ctivate = await manager.findOne(Template_Version, {
+      const version_to_activate = await manager.findOne(Template_Version, {
         where: { id: template_version_id },
         relations: ['definition'],
       });
 
-      if (!version_to_ctivate) {
+      if (!version_to_activate) {
         throw new NotFoundException('Version not found');
       }
 
       await manager.getRepository(Template_Version).update(
         {
-          definition: { id: version_to_ctivate.definition.id },
-          language_code: version_to_ctivate.language_code,
+          definition: { id: version_to_activate.definition.id },
+          language_code: version_to_activate.language_code,
         },
         { is_active: false },
       );
-      version_to_ctivate.is_active = true;
-      return await manager.save(version_to_ctivate);
+      version_to_activate.is_active = true;
+      return await manager.save(version_to_activate);
     });
   }
 
-  async create_template_version(template_version: Template_Version_Dto) {
+  async create_template_version(version: Template_Version_Dto) {
     try {
       const definition = await this.template_definition.findOneBy({
-        id: template_version.definition,
+        id: version.definition,
       });
 
       if (!definition) {
         throw new NotFoundException(
-          `Template with id ${template_version.definition} not found`,
+          `Template with id ${version.definition} not found`,
         );
       }
 
       const last_version = await this.template_version.findOne({
         where: {
           definition: {
-            id: template_version.definition,
+            id: version.definition,
           },
-          language_code: template_version.language_code,
+          language_code: version.language_code,
         },
         order: {
           version: 'DESC',
@@ -107,15 +108,15 @@ export class TemplateService {
       const new_template_version = this.template_version.create({
         definition: definition,
         version: next_version,
-        language_code: template_version.language_code,
-        content: template_version.content,
+        language_code: version.language_code,
+        content: version.content,
         is_active: false,
       });
 
       const saved_template_version =
         await this.template_version.save(new_template_version);
 
-      if (template_version.is_active === true) {
+      if (version.is_active === true) {
         return this.activate_version(saved_template_version.id);
       }
 
@@ -139,5 +140,43 @@ export class TemplateService {
     }
 
     return template_definitions;
+  }
+
+  async get_active_template(query: Active_Template_Query_Dto) {
+    try {
+      const { code, lang, type } = query;
+
+      if (
+        !code ||
+        typeof code !== 'string' ||
+        !lang ||
+        typeof lang !== 'string' ||
+        !type ||
+        typeof type !== 'string'
+      ) {
+        throw new BadRequestException('Invalid parameters in query');
+      }
+
+      const active_template = await this.template_version.findOne({
+        where: {
+          is_active: true,
+          definition: {
+            template_code: code,
+            notification_type: type,
+          },
+        },
+      });
+
+      if (!active_template) {
+        throw new NotFoundException('No active template found');
+      }
+
+      return active_template;
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Something went wrong');
+    }
   }
 }
