3e9dd1cf4a1c086ede39c6e737fa710ad44141fc
PaulAjii <paulajii134@gmail.com>
2025-11-13 17:55:51 +0100

    feat(docs): add swagger documentations

--- template-service/src/main.ts
+++ template-service/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Template Service')
+    .setDescription('Template Service that serves content for the whole notification application')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
   console.log('Connected to the database successfully...');
+  console.log(`Access the docs here: http://localhost:${PORT}/api/docs`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
