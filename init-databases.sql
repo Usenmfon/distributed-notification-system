686344f9064587097cbe198311a4c89c482501b6
Usenmfon <usenmfonuko@gmail.com>
2025-11-14 12:41:42 +0100

    chore(config): create db instance for user service database

---
+++ init-databases.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE user_service_db;
+GRANT ALL PRIVILEGES ON DATABASE user_service_db TO postgres;
+
+-- CREATE DATABASE template_service;
+-- GRANT ALL PRIVILEGES ON DATABASE template_service TO postgres;
